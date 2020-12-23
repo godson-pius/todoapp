f96742e1296b49f78c923beff99c696febd0e9d5
godson-pius <40927597+godson-pius@users.noreply.github.com>
2020-12-23 19:45:41 +0100

    Initial Commit

---
+++ src/AddForm.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class AddTodo extends Component {
+    state = {
+        content: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+    render() {
+        return(
+            <div>
+                <form>
+                    <label>Add new todo:</label>
+                    <input type="text" onChange={this.handleChange}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo;
